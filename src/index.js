644fb4698aa943e2209046d658c0854eca6e3a61
Samuel Curry <currysl@ucmail.uc.edu>
2019-11-26 20:45:41 -0500

    completed template

---
+++ src/index.js
@@ -0,0 +1,20 @@
+ const config = require('../config');
+const fastify = require('fastify')({
+  logger: config.shouldFastifyLog,
+  pluginTimeout: 60000,
+});
+const swagger = require('../swagger-config');
+
+(async () => {
+  try {
+    fastify.register(require('fastify-swagger'), swagger.options);
+    await fastify.listen(config.port, config.serverHost);
+    fastify.swagger();
+    fastify.log.info(`Server is listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+})();
+
+module.exports = fastify;
